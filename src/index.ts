beacd7229d80ded263321b651ef25a98479b88f8
Lam Nhan <lamnhan92@gmail.com>
2018-10-14 23:37:57 +0700

    update app-scripts 0.0.14

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import { AuthApiKey } from './auth-api-key';
 
 export declare const AuthApiKeyModule: {(options: IOptions): IModule};
 
-export declare const options: IOptions;
+declare const options: IOptions;
 export const moduleExports: IModule = new AuthApiKey(options);
